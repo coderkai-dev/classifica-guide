11c83235c06fc1d3fd27b4a9fef604ee1c34f242
Sandaruwan <sandaru.ny@gmail.com>
2024-05-08 12:10:35 +0530

    fix: update branch

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -16,6 +16,7 @@ const config: Config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'coderkai-dev', // Usually your GitHub org/user name.
   projectName: 'classifica-guide', // Usually your repo name.
+  deploymentBranch: 'gh-deploy',
   trailingSlash: false,
   onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
